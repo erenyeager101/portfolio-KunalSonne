2b023adbeac7f803d2a429d66603b32890c8fa77
Kunal Sonne <153419118+erenyeager101@users.noreply.github.com>
2024-10-07 01:30:34 +0530

    Update WorkData.js

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -2,26 +2,26 @@
 
 export const Work = [
     {
-        id:1,
-        name:"ISIH'24 Winner",
-        description:"I am thrilled to announce that our team, Kogaku Nexus, has secured 8th place in the Internal Smart India Hackathon at Vishwakarma Institute of Technology, competing against over 497 teams. 🎉",
-        tags:["NASAN Medical","Chitale Bandhu","ISIH"],
+        id:3,
+        name:"Published Paper in AIP preceedings via conference hosted in GMRIT institute.",
+        description:"I have successfully published my research paper in AIP preceedings journal titled IV Fluid monitoring and Air embolism detection system. 🎉",
+        tags:["AI-PTIS 24","GMRIT","Publications"],
         
-        demo:"https://www.linkedin.com/posts/anvita-chougule-a43aa828b_i-am-thrilled-to-announce-that-our-team-activity-7238107032900251649-9x7H?utm_source=share&utm_medium=member_desktop",
+        demo:"https://www.linkedin.com/in/kunal-sonne-719748322/details/certifications/1723640097260/single-media-viewer/?profileId=ACoAAFGfsgUBrQf7ayQBSFTN7VsxElNij1R_R6o",
         
     },
     {
         id:2,
-        name:"100 Days-Of-Leetcode",
-        description:"I have done something similar with my coding skills at the platform LeetCode. Coding regularly for 100 days helped me boost my confidence in my skills. Today I completed my 100th day of coding in LeetCode and received the 100 Days Badge",
-        tags:["Leetcode","DSA"],
+        name:"Front-end and responsive Web design Certification from freeCodeCamp",
+        description:"I have recieved certificate from freeCodeCamp by building 3 different projects on frontend development showing a nearly 300 hours of equivalent work. These projects stood as base for web develpoment and lcearing basics is the first step in it.",
+        tags:["Front-end development","ReactJS"],
         
-        demo:"https://www.linkedin.com/posts/activity-7208536446461218816-nS-6?utm_source=share&utm_medium=member_desktop",
+        demo:"https://www.freecodecamp.org/certification/KunalSonne/responsive-web-design",
         
         
     },
     {
-        id:3,
+        id:1,
         name:"Winner at HACKSPIRATION'24 Hackathon",
         description:"I had the opportunity to represent Team Phoenix Flames in the Solidity Scan Web3 track during this event organized by the Microsoft Learn Student Club at VIT Pune.",
         tags:["MLSC","VIT PUNE","Web3"],
