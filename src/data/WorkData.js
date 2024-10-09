7ea9b7d6d3364445619b85d437d2085aef8101ed
Kunal Sonne <153419118+erenyeager101@users.noreply.github.com>
2024-10-09 20:24:33 +0530

    Update WorkData.js

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -2,6 +2,25 @@
 
 export const Work = [
     {
+        id:1,
+        name:"Winner at HACKSPIRATION'24 Hackathon",
+        description:"I had the opportunity to represent Team Phoenix Flames in the Solidity Scan Web3 track during this event organized by the Microsoft Learn Student Club at VIT Pune.",
+        tags:["MLSC","VIT PUNE","Web3"],
+        
+        demo:"https://www.linkedin.com/in/kunal-sonne-719748322/overlay/honors/1292645586/multiple-media-viewer/?profileId=ACoAAFGfsgUBrQf7ayQBSFTN7VsxElNij1R_R6o&treasuryMediaId=1723574531307",
+        
+    },
+    {
+        id:2,
+        name:"Front-end and responsive Web design Certification from freeCodeCamp",
+        description:"Recieved certificate from freeCodeCamp by building 3 different projects on frontend development showing a nearly 300 hours of equivalent work.",
+        tags:["Front-end development","ReactJS"],
+        
+        demo:"https://www.freecodecamp.org/certification/KunalSonne/responsive-web-design",
+        
+        
+    },
+     {
         id:3,
         name:"Published Paper in AIP preceedings via conference hosted in GMRIT institute.",
         description:"I have successfully published my research paper in AIP preceedings journal titled IV Fluid monitoring and Air embolism detection system. 🎉",
@@ -10,25 +29,7 @@ export const Work = [
         demo:"https://www.linkedin.com/in/kunal-sonne-719748322/details/certifications/1723640097260/single-media-viewer/?profileId=ACoAAFGfsgUBrQf7ayQBSFTN7VsxElNij1R_R6o",
         
     },
-    {
-        id:2,
-        name:"Front-end and responsive Web design Certification from freeCodeCamp",
-        description:"I have recieved certificate from freeCodeCamp by building 3 different projects on frontend development showing a nearly 300 hours of equivalent work. These projects stood as base for web develpoment and lcearing basics is the first step in it.",
-        tags:["Front-end development","ReactJS"],
-        
-        demo:"https://www.freecodecamp.org/certification/KunalSonne/responsive-web-design",
-        
-        
-    },
-    {
-        id:1,
-        name:"Winner at HACKSPIRATION'24 Hackathon",
-        description:"I had the opportunity to represent Team Phoenix Flames in the Solidity Scan Web3 track during this event organized by the Microsoft Learn Student Club at VIT Pune.",
-        tags:["MLSC","VIT PUNE","Web3"],
-        
-        demo:"http://hackspiration.com",
-        
-    },
+    
     
     
   
