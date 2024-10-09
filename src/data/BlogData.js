b10e88b564810cbff1537186122fdf0a7ebdd273
Kunal Sonne <153419118+erenyeager101@users.noreply.github.com>
2024-10-10 01:36:41 +0530

    Update BlogData.js

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -14,7 +14,7 @@ export const Blogs = [
         name:"ML Based Heart Disease Detection System",
         tags:["Python","Machine Learning"],
         date:"5 Februaury, 2024",
-        imgSrc:"https://github.com/erenyeager101/ML-based-Heart-Disease-Detection/blob/main/pics/Q6_Image1_EDAI2.png",
+        imgSrc:"https://github.com/erenyeager101/portfolio-KunalSonne/blob/main/src/assets/Images/heart.jpg",
         link:"https://github.com/erenyeager101/ML-based-Heart-Disease-Detection"
     },
     {
@@ -22,7 +22,7 @@ export const Blogs = [
         name:"Web3 based Carbon Credit marketplace",
         tags:["Web3","Solidity","ReactJS"],
         date:"5 August, 2024",
-        imgSrc:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.investopedia.com%2Fterms%2Fc%2Fcarbon_credit.asp&psig=AOvVaw1SWBHsIh4-jOhFPnPrUpf5&ust=1728329692166000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNDtwP2_-ogDFQAAAAAdAAAAABAE",
+        imgSrc:"https://github.com/erenyeager101/portfolio-KunalSonne/blob/main/src/assets/Images/carbon.jpg",
         link:"https://github.com/erenyeager101/Carbon_marketplace"
     },
     {
@@ -30,7 +30,7 @@ export const Blogs = [
         name:"Wellness LinkHub Hospital portal",
         tags:["Django","Web Developemnt","SQL"],
         date:"26 Jan, 2024",
-        imgSrc:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fgithub.com%2Ferenyeager101%2FWellness_LinkHub&psig=AOvVaw2zdO_c4aHH340ZF56ZVpFI&ust=1728329862542000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPjVk87A-ogDFQAAAAAdAAAAABAE",
+        imgSrc:"https://github.com/erenyeager101/portfolio-KunalSonne/blob/main/src/assets/Images/hospital.jpg",
         link:"https://github.com/erenyeager101/Wellness_LinkHub"
     },
     {
@@ -38,7 +38,7 @@ export const Blogs = [
         name:"Web3 based Pizza buying Dapp ",
         tags:["Web3","Solidity","Javascript"],
         date:"3 March, 2024",
-        imgSrc:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fgithub.com%2FPizzaDAO%2Fpizza-dapp&psig=AOvVaw2Ba-mNx_tD1Ydd5EvR6-za&ust=1728329970952000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKCgxIPB-ogDFQAAAAAdAAAAABAE",
+        imgSrc:"https://github.com/erenyeager101/portfolio-KunalSonne/blob/main/src/assets/Images/pizza.jpg",
         link:"https://github.com/erenyeager101/PizzaDappBlockchain"
     },
     {
@@ -46,7 +46,7 @@ export const Blogs = [
         name:"Hotel Management using DSA in C",
         tags:["DSA","C programming"],
         date:"5 August, 2023",
-        imgSrc:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.skyfilabs.com%2Fproject-ideas%2Fhotel-management-system-project-in-c&psig=AOvVaw0-0dDaseipxyqVCGmV-qY4&ust=1728330098754000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPi_677B-ogDFQAAAAAdAAAAABAE",
+        imgSrc:"https://github.com/erenyeager101/portfolio-KunalSonne/blob/main/src/assets/Images/hotel.jpg",
         link:"https://github.com/erenyeager101/Hotel-Management-in-C"
 
     },
